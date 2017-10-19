c5fb8bb71f151eb3f2fcd8e9098d47d235ca2f7a
Baska Batbold <baskabatbold@gmail.com>
2017-10-19 16:47:35 -0500

    create survey - controller and form

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -2,6 +2,10 @@ get '/surveys/:survey_id/questions/new' do
 
 end
 
+post '/surveys/:survey_id/questions' do
+
+end
+
 get '/surveys/:survey_id/questions/:id' do
   # showing a question
 end
--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -8,6 +8,23 @@ get '/surveys/new' do
   erb :"/surveys/new"
 end
 
+post '/surveys' do
+   @survey = Survey.new(title: params[:title], creator_id: current_user.id)
+  if request.xhr?
+    if @survey.save
+      erb :"/questions/new", layout: false
+    else
+      status 422
+      @survey.errors.full_messages
+    end
+  else
+    if @survey.save
+      redirect "/surveys/new"
+    else
+      @survey.errors.full_messages
+    end
+  end
+end
 # survey
   # question
     # choice 1
