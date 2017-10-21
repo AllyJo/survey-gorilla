4cc3b5a51c5e9af16a09e0fe505ac9f2bc1588d3
Baska Batbold <baskabatbold@gmail.com>
2017-10-21 11:23:03 -0500

    survey response uniq

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,4 +2,5 @@ class Response < ActiveRecord::Base
   belongs_to :survey_taker, class_name: "User"
   belongs_to :choice
   has_one :survey, through: :choice
+  validates :survey_taker, uniqueness: true
 end
